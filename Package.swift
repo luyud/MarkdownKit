1a1551f9e639f6a4d24b8ee0c0a8d573b5997da0
Logan <luyuyx@qq.com>
2025-07-16 16:24:31 +0800

    Update Package.swift, iOS14

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MarkdownKit",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15),
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
